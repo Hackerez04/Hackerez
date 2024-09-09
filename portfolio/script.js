0dcbdaea7e0f00b945e43aa032ea8805880e4b93
Hackerez <kevinusvardi@libero.it>
2024-09-09 12:24:51 +0200

    Create script.js

---
+++ portfolio/script.js
@@ -0,0 +1,18 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
